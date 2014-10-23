78f0fcef235e7d8bc586f6adb91bb16fcceaa8fa
RocioB <rociobarlo@gmail.com>
2014-10-23 19:39:10 +0200

    Panel

---
+++ Assets/Scripts/Puntuacion.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Puntuacion : MonoBehaviour {
+	
+	private Text texto;
+	
+	void Start(){
+		texto = GameObject.Find ("TextoNivel").GetComponent<Text>();
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D col){
+		GameControl.score = GameControl.score + 1;
+		//Debug.Log (GameControl.score);
+		
+		texto.text = "Level " + GameControl.score.ToString ();
+	}
+	
+}
---
+++ Assets/Scripts/scoreUp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class scoreUp : MonoBehaviour {
+	
+	private Text texto;
+	
+	void Start(){
+		texto = GameObject.Find ("Level").GetComponent<Text>();
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D col){
+		GameControl.score = GameControl.score + 1;
+		Debug.Log (GameControl.score);
+		
+		texto.text = "Level " + GameControl.score.ToString ();
+	}
+	
+}
